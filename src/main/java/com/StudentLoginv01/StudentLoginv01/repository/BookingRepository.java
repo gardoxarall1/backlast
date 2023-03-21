279f88ec4ae4f96fa8c705efd12c1e9f76e43959
gardoxarall1 <djotuns@gmail.com>
2023-03-21 21:30:32 +0100

    first commit

---
+++ src/main/java/com/StudentLoginv01/StudentLoginv01/repository/BookingRepository.java
@@ -0,0 +1,14 @@
+package com.StudentLoginv01.StudentLoginv01.repository;
+
+import com.StudentLoginv01.StudentLoginv01.model.Booking;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+	List<Booking> findAllByStudent_IdOrderByCreatedAtDesc(Long id);
+
+	List<Booking> findAllByTutor_IdOrderByCreatedAtDesc(Long id);
+}
